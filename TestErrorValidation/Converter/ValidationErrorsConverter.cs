8f3a8ddfa411cd0f25b753754425791bb8d07ef7
liuchao862739233 <13487011994@163.com>
2023-05-24 11:36:26 +0800

    第一次提交代码

---
+++ TestErrorValidation/Converter/ValidationErrorsConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace TestErrorValidation.Converter
+{
+
+    /*
+     此转换器是为了
+       <!--<TextBlock Margin="0 3" Text="{Binding Path=(Validation.Errors)[0].ErrorContent,
+                    RelativeSource={RelativeSource AncestorType=TextBox,Mode=FindAncestor},StringFormat=*{0}}"
+                    Name="errorTxt" Grid.Row="1" />-->
+    的
+       Path=(Validation.Errors)[0].ErrorContent 会出现为null的情况
+     */
+
+    /// <summary>
+    /// 该转换器检查Validation.Errors集合是否为空，
+    /// 并根据需要返回适当的字符串
+    /// </summary>
+    public class ValidationErrorsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null && value is ReadOnlyObservableCollection<ValidationError> errors && errors.Count > 0)
+            {
+                return errors[0].ErrorContent.ToString();
+            }
+
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
---
+++ TestErrorValidation/Validation/CustomValidationRule.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+
+namespace TestErrorValidation.Validation
+{
+    public class CustomValidationRule : ValidationRule
+    {
+        /// <summary>
+        /// 待校验数据类型
+        /// </summary>
+        public string ValidateType { get; set; }
+
+        /// <summary>
+        /// 数据校验
+        /// </summary>
+        /// <param name="value">待校验数据</param>
+        /// <param name="cultureInfo"></param>
+        /// <returns></returns>
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            try
+            {
+                string valueString = value?.ToString().Trim();
+
+                if (string.IsNullOrEmpty((value ?? "").ToString()))
+                {
+                    return new ValidationResult(false, "*不能为空！");
+                }
+
+                //Int值校验
+                if (ValidateType == "IntPlusVdt")
+                {
+                    if (!int.TryParse(valueString, out int res) || res <= 0)
+                    {
+                        return new ValidationResult(false, "*该数据应为正整数！");
+                    }
+
+                    return new ValidationResult(true, null);
+                }
+
+                return new ValidationResult(true, null);
+            }
+            catch (Exception e)
+            {
+                return new ValidationResult(false, e.Message);
+            }
+        }
+    }
+}
